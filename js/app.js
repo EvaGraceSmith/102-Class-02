2dee605fc7e6a89e0b1baa3a05f0c8fc5f221272
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-02 14:05:54 -0500

    added about me file

--- js/app.js
+++ js/app.js
@@ -9,83 +9,105 @@
 // so that I feel like I am interacting with the site owner.
 
 // Ask the user their name through a prompt()
+let userName= prompt('What is your name');
+console.log('user name: ' , userName);
+
 
 // Display that name back to the user through a
 // custom greeting welcoming them to your site.
 
+alert('Hello, ' + userName + '! Let\'s have some fun! Try guessing the answers to these questions about me:');
 
-let userName= prompt('What is your name');
-console.log('user name: ' , userName);
 
-alert('Hello! ' + userName + 'let\'s have some fun! Try guessing the answers to these questions:');
+// Prompt the user a total of exactly five yes or no questions.
 
-let questionOne = prompt('Do I like pineapple on my pizza?');
-let answerOne = questionOne.toUpperCase().trim();
+// The user input for the answer must accept either y/n or yes/no
+// responses while taking into consideration case sensitivity by
+// normalizing the user input so that it can be validated
+// (hint: look into the .toUpperCase() or .toLowerCase() functions).
 
-console.log (answerOne);
+let question1 = prompt('First: Pineapple on pizza, yes or no?');
+let answer1 = question1.toUpperCase().trim();
 
-if(answerOne === 'YES' || answerOne === 'Y'){
-  alert ('The only thing better than Hawaiian pizza is eating Hawaiian pizza in Hawaii!');
-} else if (answerOne === 'NO' || answerOne ==='N'){
-  alert ('I Heart Hawaiian!');
+// Add console.log() messages within your app to notify the user
+// if they are correct. 
+
+console.log (answer1);
+// Be sure to let the user know if they answered the question
+// correctly by alerting them with a response.
+// Before submitting, comment out (don’t delete) your console.log()
+// messages and replace them with the alerts() to complete the project.
+
+if(answer1 === 'YES' || answer1 === 'Y'){
+  alert ('YES! The only thing better than Hawaiian pizza is eating Hawaiian pizza in Hawaii!');
+} else if (answer1 === 'NO' || answer1 ==='N'){
+  alert ('I Heart pineapple!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
 
-let questionTwo = prompt('Do I like camping?');
-let answerTwo = questionTwo.toUpperCase().trim();
+let question2 = prompt('Does Eva go camping?');
+let answer2 = question2.toUpperCase().trim();
 
-console.log (answerTwo);
+console.log (answer2);
 
-if(answerTwo === 'YES' || answerTwo === 'Y'){
+if(answer2 === 'YES' || answer2 === 'Y'){
   alert ('Do I ever! Campfires, hotdogs, and smores. Yes, please!');
-} else if (answerTwo === 'NO' || answerTwo ==='N'){
-  alert ('Lord grant me the serenity to accept the things I cannot change, and the wisdom to know when to go camping!');
+} else if (answer2 === 'NO' || answer2 ==='N'){
+  alert ('Lord grant me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know when to just go camping!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
 
-let questionThree = prompt('Do I like pottery?');
-let answerThree = questionThree.toUpperCase().trim();
+let question3 = prompt('Is Eva Canadian?');
+let answer3 = question3.toUpperCase().trim();
 
-console.log (answerThree);
+console.log (answer3);
 
-if(answerThree === 'YES' || answerThree === 'Y'){
-  alert ('One day I hope to have my entire dishware set be handmade');
-} else if (answerThree === 'NO' || answerThree ==='N'){
-  alert ('Throwing clay is great stress relief!');
+if(answer3 === 'N0' || answer3 === 'N'){
+  alert ('I am Iowan');
+} else if (answer3 === 'YES' || answer3 ==='Y'){
+  alert ('I do really like Maple Syrup, but I am not Canadian.');
 } else {
   alert ('Please remember enter: yes or no!');
 }
 
-let questionFour = prompt('Do I like sunsets?');
-let answerFour = questionFour.toUpperCase().trim();
+let question4 = prompt('Do I like sunsets?');
+let answer4 = question4.toUpperCase().trim();
 
-console.log (answerFour);
+console.log (answer4);
 
-if(answerFour === 'YES' || answerFour === 'Y'){
+if(answer4 === 'YES' || answer4 === 'Y'){
   alert ('I am a total sunset girl!');
-} else if (answerFour === 'NO' || answerFour ==='N'){
+} else if (answer4 === 'NO' || answer4 ==='N'){
   alert ('Iowa has amazing sunsets!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
 
-let questionFive = prompt('Have I ever lived overseas?');
-let answerFive = questionFive.toUpperCase().trim();
+let question5 = prompt('Has Eva ever lived overseas?');
+let answer5 = question5.toUpperCase().trim();
 
-console.log (answerFive);
+console.log (answer5);
 
-if(answerFive === 'YES' || answerFive === 'Y'){
+if(answer5 === 'YES' || answer5 === 'Y'){
   alert ('I have lived in both Australia and New Zealand, two amazingly beautiful countries');
-} else if (answerFive === 'NO' || answerFive ==='N'){
+} else if (answer5 === 'NO' || answer5 ==='N'){
   alert ('This girl loves to travel!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
 
-
+// Display the user’s name back to them in your final
+// message to the user.
 alert('Thank you for visiting my site, ' + userName + ' !');
+
+
+// **************END OF CODE**************
+
+
+// NOTES FROM CLASS:
+
 // //convert to lowerCase
 
 // userName.toLowerCase(); or the userName.toUpperCase();
@@ -114,46 +136,13 @@ alert('Thank you for visiting my site, ' + userName + ' !');
 // }
 
 
-
-
-
-
-
-
 // validate the input using some conditional logic
 
-
-
-
-// Prompt the user a total of exactly five yes or no questions.
-
-// The user input for the answer must accept either y/n or yes/no
-// responses while taking into consideration case sensitivity by
-// normalizing the user input so that it can be validated
-// (hint: look into the .toUpperCase() or .toLowerCase() functions).
-
-// Be sure to let the user know if they answered the question
-// correctly by alerting them with a response.
-
-// Add console.log() messages within your app to notify the user
-// if they are correct. Before submitting,
-// comment out (don’t delete) your console.log()
-// messages and replace them with the alerts() to complete the project.
 
 
 // let isExecuted = confirm("Would you like to learn more about me?");
 
 // console.log(isExecuted); // YES = true, NO = false
-
-
-
-// Did I live in New Zealand?
-// Do I have any children?
-// Do I like pineapple on my pizza?
-// The only thing better than eating Hawaiian pizza is eating it in Hawaii.
-// Are you ready for more questions?
-
-
 
 
 // let yes = "Yes";
@@ -168,7 +157,6 @@ alert('Thank you for visiting my site, ' + userName + ' !');
 
 
 
-// Display the user’s name back to them in your final
-// message to the user.
+
 
 
