b8dfdc92b68048aae01ff5dfb15305e36fe9a72f
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-04 16:06:33 -0500

    added points total

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ let answer1 = question1.toUpperCase().trim();
 
 if(answer1 === 'YES' || answer1 === 'Y'){
   alert ('YES! The only thing better than Hawaiian pizza is eating Hawaiian pizza in Hawaii!');
-  userPoints = userPoints + 1;
+  userPoints++;
 } else if (answer1 === 'NO' || answer1 ==='N'){
   alert ('I Heart pineapple!');
 } else {
@@ -33,11 +33,13 @@ let answer2 = question2.toUpperCase().trim();
 
 if(answer2 === 'YES' || answer2 === 'Y'){
   alert ('Do I ever! Campfires, hotdogs, and smores. Yes, please!');
+  userPoints++;
 } else if (answer2 === 'NO' || answer2 ==='N'){
   alert ('Lord grant me the serenity to accept the things I cannot change, the courage to change the things I can, and the wisdom to know when to just go camping!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
+console.log('did we get a point?', userPoints);
 
 let question3 = prompt('Is Eva Canadian?');
 let answer3 = question3.toUpperCase().trim();
@@ -46,11 +48,13 @@ let answer3 = question3.toUpperCase().trim();
 
 if(answer3 === 'NO' || answer3 === 'N'){
   alert ('I am Iowan');
+  userPoints++;
 } else if (answer3 === 'YES' || answer3 ==='Y'){
   alert ('I do really like Maple Syrup, but I am not Canadian.');
 } else {
   alert ('Please remember enter: yes or no!');
 }
+console.log('did we get a point?', userPoints);
 
 let question4 = prompt('Do I like sunsets?');
 let answer4 = question4.toUpperCase().trim();
@@ -59,11 +63,13 @@ let answer4 = question4.toUpperCase().trim();
 
 if(answer4 === 'YES' || answer4 === 'Y'){
   alert ('I am a total sunset girl!');
+  userPoints++;
 } else if (answer4 === 'NO' || answer4 ==='N'){
   alert ('Iowa has amazing sunsets!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
+console.log('did we get a point?', userPoints);
 
 let question5 = prompt('Has Eva ever lived overseas?');
 let answer5 = question5.toUpperCase().trim();
@@ -72,11 +78,13 @@ let answer5 = question5.toUpperCase().trim();
 
 if(answer5 === 'YES' || answer5 === 'Y'){
   alert ('I have lived in both Australia and New Zealand, two amazingly beautiful countries');
+  userPoints++;
 } else if (answer5 === 'NO' || answer5 ==='N'){
   alert ('This girl loves to travel!');
 } else {
   alert ('Please remember enter: yes or no!');
 }
+console.log('did we get a point?', userPoints);
 // **********Lab 3 Assignment************
 // As a user, I would like to be guided to an answer
 // through a series of feedback responses so that I can learn more
@@ -100,11 +108,13 @@ while(answer6 !== '4' && loopcounter<4){
     alert ('That\'s too low. Try again!');
   } else
   { alert('You are correct, the number is 4!');
+    userPoints++;
   }
   if (loopcounter===4)
   {alert('I\'m sorry, the correct answer was 4!');
   } }
 console.log(answer6);
+console.log('did we get a point?', userPoints);
 
 // // As a user, I would like to guess the answer to a question that
 // could have many possibilities so that I can have fun with with a
@@ -174,7 +184,7 @@ console.log('did we get a point?', userPoints);
 
 // Display the user’s name back to them in your final
 // message to the user.
-alert('Thank you for visiting my site, ' + userName + ' !');
+alert('Thank you for visiting my site, ' + userName + ' !' + 'You earned ' + userPoints + ' points out of 7! Congratulations!');
 
 
 // **************END OF CODE**************
